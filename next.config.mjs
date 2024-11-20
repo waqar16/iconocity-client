3b4b53bd38327faca068e6b47f99931ac0f835e8
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-11-20 17:20:54 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["cdn-icons-png.freepik.com", "lh3.googleusercontent.com"],
-//   },
-//   // images: {
-//   //   remotePatterns: [
-//   //     {
-//   //       protocol: "https",
-//   //       hostname: "cdn-icons-png.freepik.com",
-//   //       port: "",
-//   //       pathname: "",
-//   //     },
-//   //     {
-//   //       protocol: "https",
-//   //       hostname: "lh3.googleusercontent.com",
-//   //       port: "",
-//   //       pathname: "",
-//   //     },
-//   //   ],
-//   // },
-// };
-
-// export default nextConfig;
-
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn-icons-png.freepik.com",
-        pathname: "/**",  // Matches all paths within the domain
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",  // Matches all paths within the domain
-      },
-    ],
+    domains: ["cdn-icons-png.freepik.com", "lh3.googleusercontent.com"],
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "cdn-icons-png.freepik.com",
+  //       port: "",
+  //       pathname: "",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "lh3.googleusercontent.com",
+  //       port: "",
+  //       pathname: "",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;

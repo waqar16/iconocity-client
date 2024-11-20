3a9d130f282c593bcd0d0d2596a7d80645e55c54
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-11-20 17:16:43 +0500

    fixed similar icon pagination issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,48 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["cdn-icons-png.freepik.com", "lh3.googleusercontent.com"],
+//   },
+//   // images: {
+//   //   remotePatterns: [
+//   //     {
+//   //       protocol: "https",
+//   //       hostname: "cdn-icons-png.freepik.com",
+//   //       port: "",
+//   //       pathname: "",
+//   //     },
+//   //     {
+//   //       protocol: "https",
+//   //       hostname: "lh3.googleusercontent.com",
+//   //       port: "",
+//   //       pathname: "",
+//   //     },
+//   //   ],
+//   // },
+// };
+
+// export default nextConfig;
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn-icons-png.freepik.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.freepik.com",
+        pathname: "/**",  // Matches all paths within the domain
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",  // Matches all paths within the domain
+      },
+    ],
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdn-icons-png.freepik.com",
-  //       port: "",
-  //       pathname: "",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "lh3.googleusercontent.com",
-  //       port: "",
-  //       pathname: "",
-  //     },
-  //   ],
-  // },
 };
 
 export default nextConfig;
